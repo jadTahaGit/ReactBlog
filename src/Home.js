4ef44776a8f0963d79d4b2bf17ecb4eb4b227a66
Jad Taha <jadtaha@MBP-von-Jad.fritz.box>
2021-12-22 22:50:39 +0100

    version 01.00.00

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (  
+        <div className="App">
+            <Navbar></Navbar>
+            <div className="content">
+                <h2>Homepage</h2>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>JT Blog</h1>
+            <a href="/">Home</a>
+            <a href="/create" style={{
+                color: "white",
+                backgroundColor: '#f1356d',
+                borderRadius: '8px'
+
+            }}>New Blog</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
