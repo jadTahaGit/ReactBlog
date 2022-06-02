18755f3150b6ed4f112f980fd4172e1ba9fed630
Jad Taha <jadtaha.de@gmail.com>
2022-06-02 20:43:51 +0200

    Home edited --version 01.00.01

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
 import Navbar from "./Navbar";
 
 const Home = () => {
+    const handleClick = () => {
+        console.log("hello, jadis");
+    } 
+
+    const withParam = (name) => {
+        console.log("hello " + name);
+    }
     return (  
         <div className="App">
-            <Navbar></Navbar>
+            {/* <Navbar></Navbar> */}
             <div className="content">
                 <h2>Homepage</h2>
+                <button onClick={handleClick}>Click me</button>
+                <button onClick={()=>withParam("Sarah")}>Click Hieeer</button>
             </div>
         </div>
     );
